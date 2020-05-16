0ac7a2414ab8339892354998a3b54a5b57f49cba
Fabio Cagliero <caglierofabio@gmail.com>
2020-05-16 10:47:16 +0200

    Add helpers

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,41 @@
 <?php
+
+use Illuminate\Routing\UrlGenerator;
+
+if (! function_exists('i18n_route')) {
+    /**
+     * Generate the URL to a named i18n route.
+     *
+     * @param  string $name
+     * @param  mixed $parameters
+     * @param  bool $absolute
+     * @return string
+     */
+    function i18n_route(string $name, $parameters = [], $absolute = true)
+    {
+        $i18nName = app('i18n')->getRegion().".{$name}";
+
+        if (app('router')->has($i18nName)) {
+            return app(UrlGenerator::class)->route($i18nName, $parameters, $absolute);
+        }
+
+        return app(UrlGenerator::class)->route($name, $parameters, $absolute);
+    }
+}
+
+if (! function_exists('i18n_url')) {
+    /**
+     * Generate a i18n url for the application.
+     *
+     * @param  string $path
+     * @param  mixed $parameters
+     * @param  bool|null $secure
+     * @return string
+     */
+    function i18n_url(string $path, $parameters = [], $secure = null)
+    {
+        $path = app('i18n')->getRegion().'/'.ltrim($path, '/');
+
+        return app(UrlGenerator::class)->to($path, $parameters, $secure);
+    }
+}
